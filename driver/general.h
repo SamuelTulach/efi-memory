155a7141dc708f5827f755b02a39c0db86ed9bd5
Samuel Tulach <samtulach@gmail.com>
2020-07-20 22:28:28 +0200

    Update general.h

--- driver/general.h
+++ driver/general.h
@@ -5,9 +5,8 @@
 // instead of SysV ABI, we now have to do transitions
 // GNU-EFI has a functionality for this (thanks god)
 #define GNU_EFI_USE_MS_ABI 1
-#define stdcall __attribute__((stdcall)) // wHy NoT tO jUsT uSe MsVc
+#define stdcall __attribute__((ms_abi)) // wHy NoT tO jUsT uSe MsVc
 #define fastcall __attribute__((fastcall))
-// EFIAPI == __attribute__((ms_abi))
 
 // Mandatory defines
 #include <efi.h>
